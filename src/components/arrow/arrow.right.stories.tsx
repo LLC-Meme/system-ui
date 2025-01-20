78a481378486d053afbf1bf9b70cd2dc5b6f591d
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-21 05:41:41 +0900

    create each story file for arrow

---
+++ src/components/arrow/arrow.right.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Arrow from "./arrow";
+
+
+/**
+ * 右向きの矢印を表示します。
+ * 
+ * なお、プレビューとして`fill-info w-4 h-4`のクラスを設定しています。
+ * 実際に用いる際には、適切なクラスを設定してください。
+ */
+const meta = {
+  title: "Arrow/Right",
+  component: Arrow.Right,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    ref: {
+      description: "SVG要素への参照",
+      table: {
+        type: { summary: "React.Ref<SVGSVGElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Arrow.Left>;
+
+export default meta;
+
+export const Right: StoryObj<typeof Arrow.Right> = {
+  render: (args) => (
+    <Arrow.Right
+      className="fill-info w-4 h-4"
+      {...args}
+    />
+  ),
+};
