a1b791ac56e85a01fc07fd6f7162449efdac7f60
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-20 20:55:46 +0900

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
