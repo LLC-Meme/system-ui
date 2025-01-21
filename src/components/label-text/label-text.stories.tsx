6572bbd73fedc2e047b29ffdd53c96a17fe8b8de
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-21 19:35:48 +0900

    create label text

---
+++ src/components/label-text/label-text.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import LabelText from "./label-text";
+
+/**
+ * `input`、`select`などフォーム内の各アイテムに対するラベルを表現するコンポーネント。  
+ * 
+ * <span style="color:red">label要素とは違うので注意してください。</span>  
+ */
+const meta = {
+  title: "LabelText",
+  component: LabelText,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    children: {
+      description: "フォームアイテムのラベル",
+      table: {
+        type: { summary: "React.ReactNode" },
+      },
+      control: {
+        type: "text",
+      },
+    },
+    className: {
+      description: "追加のクラス名",
+      table: {
+        type: { summary: "string" },
+      },
+      control: {
+        type: "text",
+      },
+    },
+    ref: {
+      description: "span要素への参照",
+      table: {
+        type: { summary: "React.Ref<HTMLSpanElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    }
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof LabelText>;
+
+export default meta;
+type Story = StoryObj<typeof LabelText>;
+
+export const Example: Story = {
+  args: {
+    children: "フォームアイテムのラベル",
+  },
+};
