fe3d5f130f9815605ae1aebc36e776e2b5dfc8c9
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-21 21:28:27 +0900

    create radio

---
+++ src/components/radio/radio.button.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Radio from "./radio";
+
+/**
+ * ラジオボタンのボタン部分を提供します。  
+ */
+const meta = {
+  title: "Radio/Button",
+  component: Radio.Button,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    ref: {
+      description: "ラジオボタン要素への参照",
+      table: {
+        type: { summary: "React.Ref<HTMLInputElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    }
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Radio.Button>;
+
+export default meta;
+type Story = StoryObj<typeof Radio.Button>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Checked: Story = {
+  args: {
+    checked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
---
+++ src/components/radio/radio.stories.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Radio from "./radio";
+
+function RadioExample() {
+  return (
+    <Radio.Group>
+      <Radio.Label>
+        <Radio.Button />
+        <span>ラジオボタン1</span>
+      </Radio.Label>
+      <Radio.Label>
+        <Radio.Button />
+        <span>ラジオボタン2</span>
+      </Radio.Label>
+      <Radio.Label>
+        <Radio.Button />
+        <span>ラジオボタン3</span>
+      </Radio.Label>
+    </Radio.Group>
+  );
+}
+
+
+/**
+ * ラジオボタンのグループを提供します。  
+ * 
+ * `Radio.Group`、`Radio.Label`、`Radio.Button`の３つを組み合わせて使用します。  
+ */
+const meta = {
+  title: "Radio",
+  component: RadioExample,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof RadioExample>;
+
+export default meta;
+type Story = StoryObj<typeof RadioExample>;
+
+export const Example: Story = {
+  args: {},
+};
