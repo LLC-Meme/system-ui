c4f02cf884da33bccf3af1d9ab4e9ad5dc697df3
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-22 00:00:31 +0900

    create sidebar

---
+++ src/components/sidebar/sidebar.container.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Sidebar from "./sidebar";
+
+/**
+ * サイドバーのコンテナです。  
+ * 全てのサイドバーの要素はこのコンテナの中に配置します。  
+ */
+const meta = {
+  title: "Sidebar/Container",
+  component: Sidebar.Container,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    ref: {
+      description: "この要素への参照",
+      table: {
+        type: { summary: "React.Ref<HTMLElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    },
+    children: {
+      description: "表示する要素",
+      table: {
+        type: { summary: "React.ReactNode" },
+      },
+      control: {
+        type: "text",
+      },
+    },
+    isOpen: {
+      description: "サイドバーが開いているかどうかを指定します。",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: "false" },
+      },
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+  tags: ["autodocs"]
+} satisfies Meta<typeof Sidebar.Container>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Container: Story = {
+  args: {
+    children: "Container",
+    isOpen: true,
+  },
+};
---
+++ src/components/sidebar/sidebar.group-label.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Sidebar from "./sidebar";
+
+/**
+ * サイドバーのグループラベルです。
+ */
+const meta = {
+  title: "Sidebar/GroupLabel",
+  component: Sidebar.GroupLabel,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    ref: {
+      description: "この要素への参照",
+      table: {
+        type: { summary: "React.Ref<HTMLDivElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    },
+    children: {
+      description: "表示するテキスト",
+      table: {
+        type: { summary: "React.ReactNode" },
+      },
+      control: {
+        type: "text",
+      },
+    },
+  },
+  tags: ["autodocs"]
+} satisfies Meta<typeof Sidebar.GroupLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const GroupLabel: Story = {
+  args: {
+    children: "Group Label",
+  },
+};
