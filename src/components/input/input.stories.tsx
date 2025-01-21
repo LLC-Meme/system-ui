fd0b53a5f506d837fc07c1b2ae43392b4192c1b3
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-21 19:23:51 +0900

    create input

---
+++ src/components/input/input.stories.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Input from "./input";
+
+/**
+ * テキストの入力を受け付ける`input`コンポーネント  
+ */
+const meta = {
+  title: "Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    ref: {
+      description: "Input要素への参照",
+      table: {
+        type: { summary: "React.Ref<HTMLInputElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <Input placeholder="Default" />
+    );
+  }
+};
+
+export const Disabled: Story = {
+  render: () => {
+    return (
+      <Input disabled placeholder="Default" />
+    );
+  }
+};
