e9a37baef2823a76ab7dbdca4ddca582ea29eb3a
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-23 06:01:18 +0900

    create surface

---
+++ src/components/UI/surface/surface.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Surface from "./surface";
+
+/**
+ * Surfaceを表現するコンポーネントです。  
+ */
+const meta = {
+  title: "UI/Surface",
+  component: Surface,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    ref: {
+      description: "この要素への参照",
+      table: {
+        type: { summary: "React.Ref<HTMLDivElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    },
+    children: {
+      description: "Surface内に表示するコンテンツ",
+      table: {
+        type: { summary: "React.ReactNode" },
+      },
+      control: false
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Surface>;
+
+export default meta;
+type Story = StoryObj<typeof Surface>;
+
+export const Example: Story = {
+  args: {},
+  render: () => {
+    return (
+      <Surface>
+        <p>Surfaceコンポーネントのサンプル</p>
+      </Surface>
+    )
+  }
+};
---
+++ src/components/UI/surface/surface.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { cn } from "../../../lib/cn";
+
+
+export interface SurfaceProps extends React.ComponentPropsWithoutRef<"section"> {
+  children: React.ReactNode;
+}
+
+
+const Surface = React.forwardRef<HTMLDivElement, SurfaceProps>(({
+  children,
+  ...props
+}, ref) => {
+  return (
+    <section
+      ref={ref}
+      {...props}
+      className={cn(
+        props.className,
+        "bg-surface rounded-2xl px-6 py-8"
+      )}
+    >
+      {children}
+    </section>
+  );
+});
+Surface.displayName = "Surface";
+
+
+export default Surface;
