d94741929fe916b4a41534f891c76dba1c747c59
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-23 03:40:13 +0900

    update story type

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -59,7 +59,7 @@ const meta = {
 } satisfies Meta<typeof Button>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
   args: {
