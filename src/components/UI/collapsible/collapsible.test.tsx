3c1a07b71b519bf90c72b1472f6275fe73775824
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-25 04:14:08 +0900

    Create collapsible.test.tsx

---
+++ src/components/UI/collapsible/collapsible.test.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import Collapsible from "./collapsible";
+
+
+describe("Collapsible", () => {
+  function TestCollapsible() {
+    return (
+      <Collapsible.Container>
+        <Collapsible.Trigger aria-label="open-collapsible">Toggle</Collapsible.Trigger>
+        <Collapsible.Content>
+          <p>Collapsible Content</p>
+          <Collapsible.Item>Item 1</Collapsible.Item>
+          <Collapsible.Item>Item 2</Collapsible.Item>
+        </Collapsible.Content>
+      </Collapsible.Container>
+    );
+  }
+
+  it("デフォルトではContentが表示されない", () => {
+    render(<TestCollapsible />);
+
+    const content = screen.queryByText("Collapsible Content");
+    expect(content).not.toBeInTheDocument();
+  });
+
+
+  it("TriggerをクリックするとContentが表示", async () => {
+    render(<TestCollapsible />);
+    const trigger = screen.getByLabelText("open-collapsible");
+    fireEvent.click(trigger);
+
+    const content = screen.getByText("Collapsible Content");
+    expect(content).toBeInTheDocument();
+  });
+
+
+  it("Content内にItemが表示", async () => {
+    render(<TestCollapsible />);
+    const trigger = screen.getByLabelText("open-collapsible");
+    fireEvent.click(trigger);
+
+    // Triggerクリック後にItemが表示される
+    expect(screen.getByText("Item 1")).toBeInTheDocument();
+    expect(screen.getByText("Item 2")).toBeInTheDocument();
+  });
+
+
+});
