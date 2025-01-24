c536ec1a7e7005a122f570e61a1d11c2ac093f62
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-25 05:54:15 +0900

    Create main.stories.tsx

---
+++ src/components/layout/main/main.stories.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Main from "./main";
+
+const meta = {
+  title: "Layout/Main",
+  component: Main,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    ref: {
+      description: "この要素への参照",
+      table: {
+        type: { summary: "React.Ref<HTMLDivElement>" },
+        defaultValue: { summary: "undefined" },
+      },
+      control: false,
+    },
+    children: {
+      description: "Main内に表示するコンテンツ",
+      table: {
+        type: { summary: "React.ReactNode" },
+      },
+      control: false
+    },
+    sidebarOpen: {
+      description: "サイドバーが開いているかどうか",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: "false" },
+      },
+      control: false
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Main>;
+
+export default meta;
+type Story = StoryObj<typeof Main>;
+
+export const Example: Story = {
+  args: {
+    sidebarOpen: false,
+    children: (
+      <p>Mainコンポーネントのサンプル</p>
+    ),
+  }
+};
