058b78676c8fa52c4be2f6411e6262d9f972c748
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-25 05:24:43 +0900

    create basic of page

---
+++ src/page.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Page from "./page";
+
+const meta = {
+  title: "Page",
+  component: Page,
+  parameters: {
+    layout: "fullscreen",
+  }
+} satisfies Meta<typeof Page>;
+
+export default meta;
+type Story = StoryObj<typeof Page>;
+
+export const Example: Story = {
+  
+};
---
+++ src/page.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import Header from "./components/layout/header/header";
+import Sidebar from "./components/layout/sidebar/sidebar";
+import Collapsible from "./components/UI/collapsible/collapsible";
+import Breadcrumb from "./components/UI/breadcrumb/breadcrumb";
+
+export default function Page() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <div>
+
+      <Header.Container>
+        <Header.SidebarController onClick={() => setSidebarOpen(!sidebarOpen)} />
+        <h1 className="text-2xl font-bold">
+          Acme Inc.
+        </h1>
+      </Header.Container>
+
+
+      <main className="mt-16">
+
+        <Sidebar.Container isOpen={sidebarOpen} className="w-80">
+          <Sidebar.Group>
+            <Sidebar.GroupLabel>Group Label</Sidebar.GroupLabel>
+            <Sidebar.Item>Item</Sidebar.Item>
+            <Sidebar.Item current>Current Item</Sidebar.Item>
+          </Sidebar.Group>
+          <Sidebar.Group>
+            <Sidebar.GroupLabel>Group Label</Sidebar.GroupLabel>
+            <Collapsible.Container>
+              <Collapsible.Trigger>
+                Collapsible Trigger
+              </Collapsible.Trigger>
+              <Collapsible.Content>
+                <Collapsible.Item href="/item1">
+                  Collapsible Item 1
+                </Collapsible.Item>
+                <Collapsible.Item href="/item2">
+                  Collapsible Item 2
+                </Collapsible.Item>
+              </Collapsible.Content>
+            </Collapsible.Container>
+            <Collapsible.Container>
+              <Collapsible.Trigger>
+                Collapsible Trigger
+              </Collapsible.Trigger>
+              <Collapsible.Content>
+                <Collapsible.Item href="/item1">
+                  Collapsible Item 1
+                </Collapsible.Item>
+                <Collapsible.Item href="/item2">
+                  Collapsible Item 2
+                </Collapsible.Item>
+              </Collapsible.Content>
+            </Collapsible.Container>
+            <Collapsible.Container>
+              <Collapsible.Trigger>
+                Collapsible Trigger
+              </Collapsible.Trigger>
+              <Collapsible.Content>
+                <Collapsible.Item href="/item1">
+                  Collapsible Item 1
+                </Collapsible.Item>
+                <Collapsible.Item href="/item2">
+                  Collapsible Item 2
+                </Collapsible.Item>
+              </Collapsible.Content>
+            </Collapsible.Container>
+            <Collapsible.Container>
+              <Collapsible.Trigger>
+                Collapsible Trigger
+              </Collapsible.Trigger>
+              <Collapsible.Content>
+                <Collapsible.Item href="/item1">
+                  Collapsible Item 1
+                </Collapsible.Item>
+                <Collapsible.Item href="/item2">
+                  Collapsible Item 2
+                </Collapsible.Item>
+              </Collapsible.Content>
+            </Collapsible.Container>
+            <Collapsible.Container>
+              <Collapsible.Trigger>
+                Collapsible Trigger
+              </Collapsible.Trigger>
+              <Collapsible.Content>
+                <Collapsible.Item href="/item1">
+                  Collapsible Item 1
+                </Collapsible.Item>
+                <Collapsible.Item href="/item2">
+                  Collapsible Item 2
+                </Collapsible.Item>
+              </Collapsible.Content>
+            </Collapsible.Container>
+            <Collapsible.Container>
+              <Collapsible.Trigger>
+                Collapsible Trigger
+              </Collapsible.Trigger>
+              <Collapsible.Content>
+                <Collapsible.Item href="/item1">
+                  Collapsible Item 1
+                </Collapsible.Item>
+                <Collapsible.Item href="/item2">
+                  Collapsible Item 2
+                </Collapsible.Item>
+              </Collapsible.Content>
+            </Collapsible.Container>
+          </Sidebar.Group>
+        </Sidebar.Container>
+
+        <Breadcrumb.Container>
+          <Breadcrumb.Item href="/" current={false}>
+            Home
+          </Breadcrumb.Item>
+          <Breadcrumb.Separator />
+          <Breadcrumb.Item href="/dashboard" current={false}>
+            Dashboard
+          </Breadcrumb.Item>
+          <Breadcrumb.Separator />
+          <Breadcrumb.Item href="/dashboard/sales" current={false}>
+            Sales
+          </Breadcrumb.Item>
+          <Breadcrumb.Separator />
+          <Breadcrumb.Item href="/dashboard/sales/data" current={true}>
+            Data
+          </Breadcrumb.Item>
+        </Breadcrumb.Container>
+
+      </main>
+
+    </div>
+  );
+}
