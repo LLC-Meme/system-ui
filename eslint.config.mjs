028df449346a9a4410b167ff2fa86909544d43c8
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-25 22:56:30 +0900

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,20 +3,19 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } }}},
-  { languageOptions: { globals: globals.browser }},
-  { ignores: ["node_modules"] },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     settings: {
       react: {
         version: "detect"
       },
     },
   },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } }}},
+  { languageOptions: { globals: globals.browser }},
+  { ignores: ["node_modules"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
