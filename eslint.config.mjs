03844526ac93580b259822cea536a7040b8b7f8d
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-25 22:48:53 +0900

    setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } }}},
+  { languageOptions: { globals: globals.browser }},
+  { ignores: ["node_modules"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
