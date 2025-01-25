cb077784a7f771f51ae268663be63f367b91630b
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-26 00:32:18 +0900

    create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,34 @@
+export { default as Checkbox } from "./form/checkbox/checkbox";
+export { default as ErrorMessage } from "./form/error-message/error-message";
+export { default as Input } from "./form/input/input";
+export { default as Label } from "./form/label/label";
+export { default as LabelText } from "./form/label-text/label-text";
+export { default as Radio } from "./form/radio/radio";
+export { default as Selector } from "./form/selector/selector";
+export { default as Textarea } from "./form/textarea/textarea";
+
+export { default as Header } from "./layout/header/header";
+export { default as Main } from "./layout/main/main";
+export { default as Sidebar } from "./layout/sidebar/sidebar";
+
+export { default as Alert } from "./UI/alert/alert";
+export { default as Badge } from "./UI/badge/badge";
+export { default as BreadCrumb } from "./UI/breadcrumb/breadcrumb";
+export { default as Button } from "./UI/button/button";
+export { default as Calendar } from "./UI/calendar/calendar";
+export { default as Collapsible } from "./UI/collapsible/collapsible";
+export { default as Divider } from "./UI/divider/divider";
+export { default as Modal } from "./UI/modal/modal";
+export { default as Navigation } from "./UI/navigation/navigation";
+export { default as Pagination } from "./UI/pagination/pagination";
+export { default as SearchWindow } from "./UI/search-window/search-window";
+export { default as Sheet } from "./UI/sheet/sheet";
+export { default as Surface } from "./UI/surface/surface";
+export { default as Table } from "./UI/table/table";
+export { default as Tabs } from "./UI/tabs/tabs";
+export { default as ThemeSwitch } from "./UI/theme-switch/theme-switch";
+export { default as Title } from "./UI/title/title";
+export { default as Tooltip } from "./UI/tooltip/tooltip";
+
+export { default as Spacer } from "./util/spacer/spacer";
+export { default as Stack } from "./util/stack/stack";
