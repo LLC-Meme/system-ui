f546fa3f46c1c247703d02bfdc7336ba8c422ae0
J-Naish <mjyrgd2332@i.softbank.jp>
2025-01-25 22:32:56 +0900

    style the default option

--- src/components/form/selector/selector.tsx
+++ src/components/form/selector/selector.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import { cn } from "../../../lib/cn";
 
 export interface SelectorSelectProps extends React.ComponentPropsWithoutRef<"select"> {
@@ -9,18 +11,42 @@ const Select = React.forwardRef<HTMLSelectElement, SelectorSelectProps>(({
   children,
   ...props
 }, ref) => {
+
+  const [isDefault, setIsDefault] = useState(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (props.onChange){
+      props.onChange(event);
+    }
+    setIsDefault(event.target.value === "");
+  };
+
+
+  const hasDefaultOption = React.Children.toArray(children).some(
+    (child) => React.isValidElement(child) && child.type === DefaultOption
+  );
+
+  useEffect(() => {
+    if (!hasDefaultOption) {
+      setIsDefault(false);
+    }
+  }, [hasDefaultOption]);
+
+
   return (
     <div className="relative">
       <select
         ref={ref}
         {...props}
+        onChange={handleChange}
         className={cn(
           props.className,
           "appearance-none bg-transparent outline-none cursor-pointer",
           "px-4 py-2",
           "border border-border rounded-lg",
           "focus:border-info",
-          "invalid:border-alert"
+          "invalid:border-alert",
+          isDefault ? "text-foreground-muted" : "text-foreground",
         )}
       >
         {children}
