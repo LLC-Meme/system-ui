3d6242a36dd1d4982efdf32ba0b57acb787487b3
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-06 03:07:03 +0900

    add tooltip to bar chart

--- src/components/client/chart/bar-chart/bar-chart.tsx
+++ src/components/client/chart/bar-chart/bar-chart.tsx
@@ -8,8 +8,10 @@ import {
   CartesianGrid,
   YAxis,
   XAxis,
+  Tooltip,
 } from "recharts";
-import { colorMap, colorOrders } from "../utils";
+import HStack from "../../../server/stack/h-stack";
+import { colorMap, colorOrders, formatName } from "../utils";
 
 export type BarChartDataItem = {
   name: string | number | Date;
@@ -27,6 +29,48 @@ export interface BarChartProps {
 
 export const BarChart = React.forwardRef<HTMLDivElement, BarChartProps>(
   ({ className, data, hasXAxis = true, hasYAxis = true, dataKeys }, ref) => {
+    const keys = dataKeys;
+    const keyToColor: Record<string, keyof typeof colorMap> = {};
+    keys.forEach((key, index) => {
+      keyToColor[key] = colorOrders[index] || colorOrders[0];
+    });
+
+    interface BarChartTooltipPayloadItem {
+      value: number;
+      dataKey: string;
+      payload: BarChartDataItem;
+    }
+
+    const CustomTooltip = ({
+      active,
+      payload,
+    }: {
+      active?: boolean;
+      payload?: BarChartTooltipPayloadItem[];
+    }): React.JSX.Element | null => {
+      if (!active || !payload || payload.length === 0) return null;
+      const dataItem = payload[0].payload;
+      return (
+        <div className="px-4 py-2 bg-surface rounded-lg border border-border text-sm">
+          <div className="font-bold mb-1">{formatName(dataItem.name)}</div>
+          {payload.map((entry) => (
+            <HStack key={entry.dataKey} className="items-center">
+              <div
+                className="w-3 h-3 rounded-sm mr-1"
+                style={{
+                  backgroundColor: `var(--${colorMap[keyToColor[entry.dataKey]]})`,
+                }}
+              />
+              <div className="text-foreground-muted min-w-16">
+                {entry.dataKey}
+              </div>
+              <div className="font-semibold">{entry.value}</div>
+            </HStack>
+          ))}
+        </div>
+      );
+    };
+
     return (
       <div ref={ref} className={className}>
         <ResponsiveContainer width="100%" height="100%">
@@ -50,6 +94,10 @@ export const BarChart = React.forwardRef<HTMLDivElement, BarChartProps>(
               tickLine={false}
               stroke="var(--foreground-muted)"
               className="text-sm"
+            />
+            <Tooltip
+              content={<CustomTooltip />}
+              cursor={{ fill: "var(--surface-muted2)" }}
             />
             {dataKeys.map((key, index) => (
               <Bar
