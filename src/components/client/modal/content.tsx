7e3b4493b787b3bd6b5f0675a56009c0a9ea9cc0
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-06 04:08:01 +0900

    update modal content

--- src/components/client/modal/content.tsx
+++ src/components/client/modal/content.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { cn } from "../../../lib/cn";
 import { X } from "lucide-react";
-import HStack from "../../server/stack/h-stack";
 import ModalClose from "./close";
 
 function ModalOverlay({
@@ -33,17 +32,15 @@ const ModalContent = React.forwardRef<
       ref={ref}
       className={cn(
         className,
-        "fixed left-[50%] top-[50%] z-40 grid max-w-lg translate-x-[-50%] translate-y-[-50%] bg-surface pt-4 pb-6 pr-6 pl-6",
+        "fixed left-[50%] top-[50%] z-40 grid max-w-lg translate-x-[-50%] translate-y-[-50%] bg-surface p-8",
         "duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
         "rounded-2xl",
       )}
       {...props}
     >
-      <HStack className="justify-end mb-4">
-        <ModalClose aria-label="close-modal">
-          <X className="w-6 h-6" />
-        </ModalClose>
-      </HStack>
+      <ModalClose aria-label="close-modal" className="absolute top-8 right-8">
+        <X className="w-6 h-6" />
+      </ModalClose>
       {children}
     </Dialog.Content>
   </Dialog.Portal>
