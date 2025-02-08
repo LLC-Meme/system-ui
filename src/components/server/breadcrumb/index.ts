71dfbfa7c004cadfb6f915abd09e482be2e97ad3
J-Naish <mjyrgd2332@i.softbank.jp>
2025-02-08 20:56:49 +0900

    export home

--- src/components/server/breadcrumb/index.ts
+++ src/components/server/breadcrumb/index.ts
@@ -1,3 +1,4 @@
 export { default as BreadcrumbContainer } from "./container";
+export { default as BreadcrumbHome } from "./home";
 export { default as BreadcrumbItem } from "./item";
 export { default as BreadcrumbSeparator } from "./separator";
